d764011a05105f55fb47cc7c4e7097b851c74091
Vishal <officalmailakp@gmail.com>
2023-04-22 12:19:39 +0530

    Complete Todo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Todo from "./components/Todo";
+
+function App() {
+
+  return (
+    <div className="container">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
